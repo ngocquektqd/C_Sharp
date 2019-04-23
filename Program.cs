53caf654a11b54e454c37971a9b21234010ae458
ngocquektqd <43490480+ngocquektqd@users.noreply.github.com>
2019-04-23 23:06:18 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,34 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment1
+namespace Fibonacci
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n, number;
-            Console.WriteLine("Tim cac so nguyen to nho hon n\n");
-            Console.WriteLine("Nhap vao gia tr n=");
+            int n;
+            int f1=0, f2=1, f3;
+            int sum = 1;
+            Console.WriteLine("Nhap n= ");
             n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n");
-            for (number=1; number<= n; number ++)
+            Console.WriteLine("Tinh tong " + n + " so hang dau tien cua day Fibonacci");
+            for (int i = 2; i < n; i++)
             {
-                int dem = 0;
-                for(int i=1; i<= number/2; i++)
-                {
-                    if(number % i == 0)
-                    {
-                        dem++;
-                    }
-                }
-                if (dem == 1)
-                {
-                    Console.WriteLine(+number+" La so nguyen to\n");
-                }
+                f3 = f1 + f2;
+                sum = sum + f3;
+                f1 = f2;
+                f2 = f3;
             }
+            Console.WriteLine("" + sum);
             Console.ReadKey();
         }
-
     }
 }
